4cd1ca754b10a5b460b7549e8bf05f7da0b2de4e
Viktor Skrypnyk <neovi1308@gmail.com>
2020-02-13 16:42:33 +0200

    List Demo new class

---
+++ src/main/java/com/viktorskr/javacore/chapter18/ArrayListDemo.java
@@ -0,0 +1,31 @@
+package com.viktorskr.javacore.chapter18;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String args[]){
+
+        //Создать Списочьный масив
+
+        ArrayList<String> al = new ArrayList<String>();
+
+        System.out.println("Начальтный размер Списочного масива al " + al.size());
+
+        al.add("A");
+        al.add("B");
+        al.add("C");
+        al.add("D");
+        al.add("E");
+        al.add("F");
+        al.add("G");
+        al.add(1, "A2");
+        al.add(3, "B2");
+        System.out.println("Размер масива после ввода елементов " + al + " размер " + al.size());
+
+        al.remove("F");
+        al.remove(2);
+
+        System.out.println("Вывод месива после удаления элементов " + al + " размер " + al.size());
+
+    }
+ }
