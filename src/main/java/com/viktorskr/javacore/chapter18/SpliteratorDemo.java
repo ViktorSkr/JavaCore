c82fea68dd6da0a7ef41fea27e922b13580b446f
Viktor Skrypnyk <neovi1308@gmail.com>
2020-03-22 11:58:35 +0200

    Chapter 18

--- src/main/java/com/viktorskr/javacore/chapter18/SpliteratorDemo.java
+++ src/main/java/com/viktorskr/javacore/chapter18/SpliteratorDemo.java
@@ -1,4 +1,29 @@
 package com.viktorskr.javacore.chapter18;
 
+//Применение интерфейса Spliterator
+
+import java.util.ArrayList;
+import java.util.Spliterator;
+
 class SpliteratorDemo {
+    public static void main(String args[]){
+        //Создать списочный массив типа double
+        ArrayList<Double> vals = new ArrayList<>();
+
+        //ввести значение в списочьный массив
+        vals.add(1.0);
+        vals.add(2.0);
+        vals.add(3.0);
+        vals.add(4.0);
+        vals.add(5.0);
+
+        //Вызвать метод tryAdvance() для вывода содержимого
+        //списочного массива vals
+        System.out.print("Содержимое списочного масива vals:\n");
+        Spliterator<Double> spltitr = vals.spliterator();
+        while (spltitr.tryAdvance((n) -> System.out.println(n)));
+        System.out.println();
+
+        //
+    }
 }
