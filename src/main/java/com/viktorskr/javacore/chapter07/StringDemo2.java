110ff5f13646961e0173f94bedcd37cf65d9b406
Viktor Skrypnyk <neovi1308@gmail.com>
2020-11-03 22:38:52 +0200

    New class for JavaCore

--- src/main/java/com/viktorskr/javacore/chapter07/StringDemo2.java
+++ src/main/java/com/viktorskr/javacore/chapter07/StringDemo2.java
@@ -1,4 +1,22 @@
 package com.viktorskr.javacore.chapter07;
 
 class StringDemo2 {
+    public static void main(String[] args) {
+        String strOb1 = "Первая строка";
+        String strOb2 = "Вторая строка";
+        String strOb3 = strOb1;
+
+        System.out.println("Длина строки  StrOb1 = " + strOb1.length());
+        System.out.println("Символ по индексу 3 = " + strOb1.charAt(3));
+
+        if (strOb1.equals(strOb2))
+            System.out.println("strOb1 = StrOb2");
+        else
+            System.out.println("strOb1 != StrOb2");
+
+        if (strOb1.equals(strOb3))
+            System.out.println("strOb1 = StrOb3");
+        else
+            System.out.println("strOb1 != StrOb3");
+    }
 }
