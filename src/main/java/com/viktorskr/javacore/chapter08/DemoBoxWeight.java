6444b0980db20f928812b8553b798ec0378ede87
Viktor Skrypnyk <neovi1308@gmail.com>
2020-11-12 14:27:57 +0200

    Adding class

--- src/main/java/com/viktorskr/javacore/chapter08/DemoBoxWeight.java
+++ src/main/java/com/viktorskr/javacore/chapter08/DemoBoxWeight.java
@@ -4,6 +4,8 @@ package com.viktorskr.javacore.chapter08;
 // для расширения класса Box
 
 
+import jdk.internal.platform.cgroupv1.SubSystem;
+
 class Box {
         double width;
         double height;
@@ -90,6 +92,16 @@ class  RefDemo {
         double vol;
 
         vol = weightbox.volume();
-        System.out.println("");
+        System.out.println("Объем weightbox рамен " + vol);
+        System.out.println("Вес weightbox равен " + weightbox.weight);
+        System.out.println();
+
+        plainbox = weightbox;
+        vol = plainbox.volume(); //Верно так как метод volume() определен в класе Box
+        System.out.println("Объем plainbox равен " + vol);
+
+        /* Следующий оператор ошибочен, поскольку член plainbox не определяет член weight.
+        System.out.println("Вес plainbox равен " + plainbox.weight);
+         */
     }
 }
