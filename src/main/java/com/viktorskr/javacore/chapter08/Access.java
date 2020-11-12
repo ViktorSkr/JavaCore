267333ba9c6113ae03968ae2c12b081414515614
Viktor Skrypnyk <neovi1308@gmail.com>
2020-11-12 12:57:46 +0200

    Adding class

--- src/main/java/com/viktorskr/javacore/chapter08/Access.java
+++ src/main/java/com/viktorskr/javacore/chapter08/Access.java
@@ -13,16 +13,16 @@ class A3 {
 class B3 extends A3 {
     int total;
 
-    void sum() {
+    /* void sum() {
         total = i + j;
-    }
+    } */
 }
 
 class Access {
     public static void main(String args[]) {
         B3 subOb = new B3();
         subOb.setij(10, 12);
-        subOb.sum();
+        // subOb.sum();
         System.out.println("Сумма равна " + subOb.total);
     }
 }
---
+++ src/main/java/com/viktorskr/javacore/chapter08/DemoBoxWeight.java
@@ -0,0 +1,90 @@
+package com.viktorskr.javacore.chapter08;
+
+// В этом примере наследование применяется
+// для расширения класса Box
+
+
+class Box {
+        double width;
+        double height;
+        double depth;
+
+        // Сконструировать клон объекта
+        Box (Box ob) {
+            width = ob.width;
+            height = ob.height;
+            depth = ob.depth;
+        }
+
+        //Конструктор применяемый при указании всех объектов
+        Box (double w, double h, double d) {
+            width = w;
+            height = h;
+            depth = d;
+        }
+
+        //Конструктор применяемый в отсутствии размеров
+        Box() {
+            width = -1;
+            height = -1;
+            depth = -1;
+        }
+
+        //Конструктор применяемый при создании куба
+        Box(double len) {
+            width = height = depth = len;
+        }
+
+        //Расчитать и возвратить объем
+        double volume() {
+            return  width * height * depth;
+        }
+}
+// Расширить класс Box включив в него поле веса
+class BoxWeight extends Box {
+    double weight;
+
+    //Конструктор BoxWeight
+    BoxWeight(double w, double h, double d, double m) {
+        width = w;
+        height = h;
+        depth = d;
+        weight = m;
+    }
+}
+
+//Етот класс расширяет класс Box, включая в него свойство цвета
+class ColorBox extends Box{
+    int color;
+
+    ColorBox(double w, double h, double d, int c) {
+        width = w;
+        height = h;
+        depth = d;
+        color = c;
+    }
+}
+
+class DemoBoxWeight {
+    public static void main(String args[]) {
+        BoxWeight mybox1 = new BoxWeight(10, 20, 15, 34.3);
+        BoxWeight mybox2 = new BoxWeight(2,3,4,0.076);
+        double vol;
+
+        vol = mybox1.volume();
+        System.out.println("Объем mybox1 равен " + vol);
+        System.out.println("Вес mybox1 равен " + mybox1.weight);
+        System.out.println();
+
+        vol = mybox2.volume();
+        System.out.println("Объем mybox2 равен " + vol);
+        System.out.println("Вес mybox2 равен " + mybox2.weight);
+        System.out.println();
+    }
+}
+
+class  RefDemo {
+    public static void main(String args[]) {
+
+    }
+}
