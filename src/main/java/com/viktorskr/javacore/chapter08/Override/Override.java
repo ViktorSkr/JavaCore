083667dc39f7732516e5d1c3259afd1afd78d83d
Viktor Skrypnyk <neovi1308@gmail.com>
2020-11-12 20:44:33 +0200

    Adding class Override

---
+++ src/main/java/com/viktorskr/javacore/chapter08/Override/Override.java
@@ -0,0 +1,34 @@
+package com.viktorskr.javacore.chapter08.Override;
+
+// Переопределение методов
+
+class A {
+    int i, j;
+
+    A(int a, int b) {
+        i = a;
+        j = b;
+    }
+
+    void show() {
+        System.out.println("i & j: " + i + " " + j);
+    }
+}
+class B extends A {
+    int k;
+
+    B(int a, int b, int c) {
+        super(a, b);
+        k = c;
+    }
+    void show() {
+        System.out.println("k: " + k);
+    }
+}
+
+class Override {
+    public static void main(String args[]) {
+    B subOb = new B(1, 2, 3);
+    subOb.show();
+    }
+}
